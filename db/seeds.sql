b396b32958f141ac1dbd4e74ee296e6471a6d22e
Maya Chisem <chisem.maya@gmail.com>
2020-10-02 14:52:04 -0700

    adding express, mySql databse, and other js fumctions

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheesy Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Triple Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Tastee Burger', true);
